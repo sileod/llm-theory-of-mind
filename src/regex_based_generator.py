0a5ba4b1012669b0ae4ddd84044c148ec9618458
Antoine Lernould <antoine.lernould.etu@univ-lille.fr>
2023-01-31 16:30:58 +0100

    documentation + dropbox interaction

--- src/regex_based_generator.py
+++ src/regex_based_generator.py
@@ -1,25 +1,57 @@
 import exrex
 import pandas as pd
-import dropbox
+import sys
+from util import to_dropbox, get_url_of_file
 
+# Read the names from the file
 with open('./names.txt', 'r') as f:
     names = f.read().split('\n')
 
+# Read the token from the file
+with open('./token.txt', 'r') as f:
+    token = f.read()
+
 def generate_vars(n_vars):
+    """
+    Generate a list of variables from 1 to n_vars
+    :param n_vars: the number of variables
+    :return: a list of variables
+    """
     return list(range(1, n_vars+1))
 
 def generate_agents(n_agents):
+    """
+    Return the first n_agents names
+    :param n_agents: the number of agents
+    :return: a list of names
+    """
     return names[:n_agents]
 
 def generate_law():
+    """
+    """
     return exrex.getone('Top')
 
 def generate_observations_regex(vars, agents, n_observations):
+    """
+    Generates a regex that matches the declaration of observations
+    :param vars: the list of variables
+    :param agents: the list of agents
+    :param n_observations: the number of observations
+    :return: a regex
+    """
     vars_regex   = '|'.join(map(str, vars))
     agents_regex = '|'.join(agents)
     return '((' + agents_regex + '):('+ vars_regex +')(,('+ vars_regex + ')){' + str(n_observations-1) + '} ){' + str(len(agents)) + '}'
 
 def generate_announcement_regex(vars, agents, n_announcements, statement_size):
+    """
+    Generates a regex that matches the declaration of announcements
+    :param vars: the list of variables
+    :param agents: the list of agents
+    :param n_announcements: the number of announcements
+    :param statement_size: the size of the statements
+    """
     agents_regex = '|'.join(agents)
     vars_regex   = '|'.join(map(str, vars))
 
@@ -29,7 +61,16 @@ def generate_announcement_regex(vars, agents, n_announcements, statement_size):
 
     return reg
 
-def generate_problem_regex(n_vars, n_agents, n_observations):
+def generate_problem_regex(n_vars, n_agents, n_observations, n_announcements, statement_size):
+    """
+    Generates a regex that matches an epistemic problem
+    :param n_vars: the number of variables
+    :param n_agents: the number of agents
+    :param n_observations: the number of observations per agent
+    :param n_announcements: the number of announcements
+    :param statement_size: the size of the statements
+    :return: a regex
+    """
     vars = generate_vars(n_vars)
     agents = generate_agents(n_agents)
 
@@ -37,42 +78,53 @@ def generate_problem_regex(n_vars, n_agents, n_observations):
 
     observations_regex = generate_observations_regex(vars, agents, n_observations)
 
-    announcements_regex = generate_announcement_regex(vars, agents, n_announcements=1, statement_size=3)
+    announcements_regex = generate_announcement_regex(vars, agents, n_announcements, statement_size)
 
-    problem_regex = f"VARS {','.join(map(str, vars))} LAW {law_regex} OBS {observations_regex} VALID\? {announcements_regex}\n"
+    problem_regex = f"VARS {','.join(map(str, vars))} LAW {law_regex} OBS {observations_regex} VALID\? {announcements_regex}"
 
     return problem_regex
 
+def generate_problems_to_csv(path, result_path, n_problems, n_vars=3, n_agents=3, n_observations=2, n_announcements=1, statement_size=3):
+    problem_regex = generate_problem_regex(n_vars, n_agents, n_observations, n_announcements, statement_size)
 
-with open('./token.txt', 'r') as f:
-    token = f.read()
+    print(f'Generating {n_problems} problems...')
 
-def to_dropbox(dataframe, path, token):
-    dbx = dropbox.Dropbox(token)
+    nb_generated = 0
 
-    df_string = dataframe.to_csv(index=False)
-    db_bytes = bytes(df_string, 'utf8')
+    df = pd.DataFrame(columns=['problem'])
+    for _ in range(n_problems):
+        pb = exrex.getone(problem_regex)
+        df_pb = pd.DataFrame([pb], columns=['problem'])
+
+        df = pd.concat([df, df_pb], ignore_index=True)
+
+        # Print the progress
+        nb_generated += 1
+        if nb_generated % (n_problems//10) == 0:
+            print(f'{nb_generated/n_problems*100} % generated')
+
+    print('Generating done')
+
+    print('Uploading to DropBox...')
+
+    # Upload the file to Dropbox
+    to_dropbox(df, f'/{result_path}', token)
+
+    print('Upload done')
     
-    dbx.files_upload(
-        f=db_bytes,
-        path=path,
-        mode=dropbox.files.WriteMode.overwrite
-    )
+    # Get url of the uploaded file
+    url = get_url_of_file(result_path, token)
 
+    print(f'{result_path} uploaded to DropBox : {url}')
+
+    return url
 
 if __name__ == '__main__':
-    problem_regex = generate_problem_regex(n_vars=3, n_agents=3, n_observations=2)
+    path = sys.argv[1]
+    result_path = sys.argv[2]
+    n_problems = int(sys.argv[3])
 
-    with open('./problems.txt', 'w') as f:
-        df = pd.DataFrame(columns=['problem'])
-        for _ in range(20000):
-            pb = exrex.getone(problem_regex)
-            df_pb = pd.DataFrame([pb], columns=['problem'])
-
-            df = pd.concat([df, df_pb], ignore_index=True)
-        df.to_csv(f, index=False)
-
-    to_dropbox(df, '/Applications/modlog/problems2.csv', token)
+    generate_problems_to_csv(path, result_path, n_problems)
 
 
 
